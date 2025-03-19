35434c96256059bd2712f7ce2c3d832d1cb25499
Brandon Winger-Air <brandonwinger-air@outlook.com>
2025-03-18 20:10:05 -0400

    organize & resolve server directory files

--- server.js
+++ server.js
@@ -1,29 +1,52 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const { exec } = require("child_process"); // Import child_process for opening the browser
+const { exec } = require("child_process"); // For opening the browser
+
+const isPackaged = typeof process.pkg !== "undefined";
+const basePath = isPackaged ? path.dirname(process.execPath) : __dirname;
 
 const server = http.createServer((req, res) => {
-  let filePath = "./index.html";
-  if (req.url !== "/") filePath = "." + req.url;
+  let filePath = path.join(basePath, "assets", "index.html");
 
+  // Dynamically resolve file paths based on the URL
+  if (req.url !== "/") {
+    filePath = path.join(basePath, "assets", req.url);
+  }
+
+  // Determine the file extension to set the appropriate Content-Type
   const extname = path.extname(filePath);
-
   let contentType;
-  if (extname === ".html") contentType = "text/html";
-  else if (extname === ".css") contentType = "text/css";
-  else if (extname === ".js") contentType = "application/javascript";
-  else contentType = "text/plain";
 
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("File not found");
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf-8");
-    }
-  });
+  switch (extname) {
+    case ".html":
+      contentType = "text/html";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "application/javascript";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+    case ".jpeg":
+      contentType = "image/jpeg";
+      break;
+    default:
+      contentType = "text/plain";
+  }
+
+  try {
+    const content = fs.readFileSync(filePath);
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content, "utf-8");
+  } catch (err) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("File not found");
+  }
 });
 
 // Start server on port 3000
